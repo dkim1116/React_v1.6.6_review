8630691c1392e5b6cafe71fbde6c2481f4624d86
dkim1116 <daniel.kim1116@gmail.com>
2019-01-08 18:42:30 -0800

    Replaced submit handler with a function in a parent component to pass down to SearchBar child component as a prop

--- retrieval/src/components/SearchBar.js
+++ retrieval/src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"/>
+                        <input 
+                            type="text"
+                            value={this.state.term}
+                            onChange={ (event) => this.setState({ term: event.target.value }) } />
                     </div>
                 </form>
             </div>
