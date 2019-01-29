ef5695fb86e4771380941b261fd86ad7e8daf7c1
Daniel Kim <daniel.kim1116@gmail.com>
2019-01-28 17:34:33 -0800

    Basic setup for the app

---
+++ tic-tac-toe/src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Row from './Row';
+
+class Board extends React.Component {
+    render() {
+        return <Row />;
+    }
+}
+
+export default Board;
---
+++ tic-tac-toe/src/components/Row.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Square from './Square';
+
+const Row = () => {
+    return <Square />
+}
+
+export default Row;
